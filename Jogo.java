0faba8f9f2a25ebbf2f475224d0a12d936ee0d08
Isabel Santos <isa.otakukawaii@gmail.com>
2024-10-08 21:41:05 -0300

    feat(p1): implementa batalha entre personagens

--- Jogo.java
+++ Jogo.java
@@ -1,93 +1,69 @@
 import java.util.Random;
- 
-public class Jogo{
- 
-    public static void main(String[] args)throws Exception {
- 
-        var gerador = new Random(); //cria um gerador
- 
+
+public class Jogo {
+
+    public static void main(String[] args) throws Exception {
+
+        // Exibir anúncio antes da criação dos personagens
+        Personagem.exibirAnuncio();
+
+        var gerador = new Random(); // cria um gerador
+
         int acaoDoplayer1; // declarar variáveis
- 
         int acaoDoplayer2;
- 
-        int vivo1 = 1, vivo2 = 1; //principal fator de ocorrer o looping infinito
- 
+
         Personagem player1 = new Personagem();
- 
-        Personagem player2 = new Personagem(2,10,10);
- 
-        player1.nome= "Kratos";
- 
+        Personagem player2 = new Personagem(2, 10, 10);
+
+        player1.nome = "Kratos";
         player2.nome = "Ze Preguica";
- 
-        while(vivo1 > 0 || vivo2 > 0){
-            if(vivo1 > 0){
-            acaoDoplayer1 = gerador.nextInt(3); // gerador de 3 numeros (0,1,2)
- 
-            if (acaoDoplayer1 == 0) {
- 
-                player1.cacar();
- 
-            } else if (acaoDoplayer1 == 1) {
- 
-                player1.comer();
- 
-            } else if (acaoDoplayer1 == 2) {
- 
-                player1.dormir();
- 
+
+        while (true) {
+            if (player1.getenergia() > 0) {
+                acaoDoplayer1 = gerador.nextInt(4); // gerador de 4 números (0,1,2,3)
+
+                if (acaoDoplayer1 == 0) {
+                    player1.cacar();
+                } else if (acaoDoplayer1 == 1) {
+                    player1.comer();
+                } else if (acaoDoplayer1 == 2) {
+                    player1.dormir();
+                } else if (acaoDoplayer1 == 3 && player2.getenergia() > 0) { // Verifica se o player2 está vivo antes de atacar
+                    player1.atacar(player2); // Chama o método atacar da classe Personagem
+                }
+
+                System.out.println(player1); // mostra status do player1
+                System.out.println("****************************");
+                Thread.sleep(5000); // espera 5 segundos para realizar a próxima instrução
             }
- 
-            System.out.println(player1); // mostra status do player1
- 
-            System.out.println("****************************");
-            //Thread.sleep(5000); // espera 5 segundos para realizar a próxima instrução
-           
-        }
-           
-            if(vivo2 > 0){
- 
-            acaoDoplayer2 = gerador.nextInt(10) + 1; // gerador de 10 números (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
- 
-            if (acaoDoplayer2 >= 1 && acaoDoplayer2 <= 6) { // 60%
- 
-                player2.dormir();
- 
-            } else if (acaoDoplayer2 > 6 && acaoDoplayer2 <= 9) { // 30%
- 
-                player2.comer();
- 
-            } else if (acaoDoplayer2 == 10) { // 10%
- 
-                player2.cacar();
- 
+
+            if (player2.getenergia() > 0) {
+                acaoDoplayer2 = gerador.nextInt(11); // gerador de 11 números (0 a 10)
+
+                if (acaoDoplayer2 >= 1 && acaoDoplayer2 <= 6) { // 60%
+                    player2.dormir();
+                } else if (acaoDoplayer2 > 6 && acaoDoplayer2 <= 9) { // 30%
+                    player2.comer();
+                } else if (acaoDoplayer2 == 10) { // 10%
+                    player2.cacar();
+                } else if (acaoDoplayer2 == 0 && player1.getenergia() > 0) { // Verifica se o player1 está vivo antes de atacar
+                    player2.atacar(player1); // Chama o método atacar da classe Personagem
+                }
+
+                System.out.println(player2); // mostra status do player2
+                System.out.println("****************************");
+                Thread.sleep(5000); // espera 5 segundos para realizar a próxima instrução
             }
- 
-            System.out.println(player2); // mostra status do player2
- 
-            System.out.println("****************************");
- 
-            Thread.sleep(5000); // espera 5 segundos para realizar a próxima instrução
-        }
-            //Morte do Personagem
- 
-            if (player1.getenergia() <= 0 && player2.getenergia() <= 0){
- 
+
+            // Morte do Personagem
+            if (player1.getenergia() <= 0 && player2.getenergia() <= 0) {
                 System.out.println("Players Died!");
- 
                 break;
- 
-            } else if (player1.getenergia() <= 0){
- 
-                System.out.println("Player1 Died!\n");                
-                vivo1 = 0;
-            } else if ( player2.getenergia() <= 0){
- 
+            } else if (player1.getenergia() <= 0) {
+                System.out.println("Player1 Died!\n");
+            } else if (player2.getenergia() <= 0) {
                 System.out.println("Player2 Died!\n");
-                vivo2 = 0;
             }
         }
     }
- 
 }
- 
--- Personagem.java
+++ Personagem.java
@@ -1,73 +1,100 @@
-public class Personagem{
-  String nome;
-  private int energia;
-  private int fome;
-  private int sono;
+import java.util.Random;
 
-  Personagem(){
-    System.out.println("Construindo novo personagem");
-    energia = 10;
-    fome = 0;
-    sono = 0;
-  }
+public class Personagem {
+    String nome;
+    private int energia;
+    private int fome;
+    private int sono;
 
-
-  Personagem(int energia, int fome, int sono) {
-    System.out.println("Construindo novo personagem");
-    this.energia = energia < 0 || energia > 10 ? 10 : energia;
-    this.fome = fome >= 0 && fome <= 10 ? fome : 0;
-    this.sono = sono >= 0 && sono <= 10 ? sono : 0;
-  }
-
-  void cacar(){
-    if(energia >= 2){
-      System.out.printf("%s caçando\n", nome);
-      energia -= 2; // energia = energia - 2
-    }else{
-      System.out.printf(
-        "%s sem energia para caçar\n",
-        nome
-      );
+    Personagem() {
+        System.out.println("Construindo novo personagem");
+        energia = 10;
+        fome = 0;
+        sono = 0;
     }
-    // fome +=1 fome++ ++fome
-    if (fome < 10) fome = fome + 1; 
-    // sono = sono + 1 > 10 ? sono : sono + 1;
-    sono = sono == 10 ? sono : sono + 1;
-  }
 
-  //método comer
-  void comer(){
-    if(fome >= 1){
-      System.out.println(nome + " comendo");
-      energia = Math.min(energia + 1, 10);
-      fome--;
+    Personagem(int energia, int fome, int sono) {
+        System.out.println("Construindo novo personagem");
+        this.energia = energia < 0 || energia > 10 ? 10 : energia;
+        this.fome = fome >= 0 && fome <= 10 ? fome : 0;
+        this.sono = sono >= 0 && sono <= 10 ? sono : 0;
     }
-    else{
-      System.out.println(nome + " sem fome");
-    }
-  }
-  //método dormir
-  void dormir(){
-    if(sono >= 1){
-      System.out.print(nome + " dormindo\n");
-      energia = energia == 10 ? energia : energia + 1;
-      sono -= 1;
-    }else{
-      System.out.println(nome + " sem sono");
-    }
-  }
 
-  public String toString(){
-    //nome: e:5, f:4, s:8
-    return String.format(
-      "%s: e:%d, f:%d, s:%d",
-      nome, energia, fome, sono
-    );  
-  }
-  public int getenergia (){
-    return energia;
-  }
-  public void setEnergia(int energia) {
-    this.energia = energia;
-  }
+    static void exibirAnuncio() {
+        String[] anuncios = {
+            "Coca cola *anuncio da Coca cola*",
+            "O personagem morre. *anuncio de plano de saúde*",
+            "'Cama King Size por apenas 12x com juros RS: 99,90 recupere dois de energia com essa cama. *PNG de Cama*",
+            "Fique livre de anuncios por apenas RS: 29,99 por mês!!! *anuncio do jogo*",
+            "Novo Personagem Rank SSS: *PNG do personagem SSShrek*",
+            "Tigrinho está pagando muito bem!!! *Diz dono do Tigrinho antes de ser preso por dar golpe em usuarios*",
+            "Clique aqui para ganhar muito dinheiro!!! *clonacartao.com.br*"
+        };
+        Random gerador = new Random();
+        int indice = gerador.nextInt(anuncios.length);
+        System.out.println(anuncios[indice]);
+        try {
+            Thread.sleep(30000); // Espera 30 segundos
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void cacar() {
+        if (energia >= 2) {
+            System.out.printf("%s caçando\n", nome);
+            energia -= 2; // energia = energia - 2
+        } else {
+            System.out.printf("%s sem energia para caçar\n", nome);
+        }
+        if (fome < 10) fome = fome + 1;
+        sono = sono == 10 ? sono : sono + 1;
+    }
+
+    void comer() {
+        if (fome >= 1) {
+            System.out.println(nome + " comendo");
+            energia = Math.min(energia + 1, 10);
+            fome--;
+        } else {
+            System.out.println(nome + " sem fome");
+        }
+    }
+
+    void dormir() {
+        if (sono >= 1) {
+            System.out.print(nome + " dormindo\n");
+            energia = energia == 10 ? energia : energia + 1;
+            sono -= 1;
+        } else {
+            System.out.println(nome + " sem sono");
+        }
+    }
+
+    void atacar(Personagem outro) {
+        if (energia > 0 && outro.getenergia() > 0) { // Verifica se ambos os personagens estão vivos
+            System.out.println(nome + " ataca " + outro.nome + "!");
+            outro.setEnergia(outro.getenergia() - 1);
+            System.out.println(outro.nome + " agora tem " + outro.getenergia() + " de energia.");
+            if (outro.getenergia() <= 0) {
+                System.out.println(outro.nome + " morreu!");
+            }
+        } else if (energia <= 0) {
+            System.out.println(nome + " não pode atacar porque está morto.");
+        } else {
+            System.out.println(outro.nome + " já está morto.");
+        }
+    }
+
+    public String toString() {
+        return String.format("%s: e:%d, f:%d, s:%d", nome, energia, fome, sono);
+    }
+
+    public int getenergia() {
+        return energia;
+    }
+
+    public void setEnergia(int energia) {
+        this.energia = energia;
+    }
 }
